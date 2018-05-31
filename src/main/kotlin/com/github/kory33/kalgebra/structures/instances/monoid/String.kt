b19acbe0865938b34b3ee43b2875ef12f756cd16
kory33 <6561358+kory33@users.noreply.github.com>
2018-05-31 15:15:00 +0800

    Fix implementation of String monoid

--- src/main/kotlin/com/github/kory33/kalgebra/structures/instances/monoid/String.kt
+++ src/main/kotlin/com/github/kory33/kalgebra/structures/instances/monoid/String.kt
@@ -2,17 +2,16 @@ package com.github.kory33.kalgebra.structures.instances.monoid
 
 import com.github.kory33.kalgebra.operations.MonoidOperation
 import com.github.kory33.kalgebra.structures.Monoid
+import com.github.kory33.kalgebra.structures.Multiplicative
 
-class StringMonoid(value: String): Monoid<String, StringMonoid>(value, StringMonoid) {
+class StringMonoid(value: String): Monoid<String, StringMonoid>(value), Multiplicative<StringMonoid, StringMonoid> {
 
+    override val operation = StringMonoid
     override fun lift(value: String) = StringMonoid(value)
 
     companion object: MonoidOperation<String> {
-
         override fun invoke(p1: String, p2: String) = p1 + p2
-
         override val identity = ""
-
     }
 
 }
